c3a8412536cd23751668eb89799d200b286ee0e5
ofer <ofergavri@gmail.com>
2022-12-25 21:49:43 +0200

    add new style for header, about page and home page

--- pages/home.jsx
+++ pages/home.jsx
@@ -1,9 +1,10 @@
 
 export function Home() {
     return (
-        <section>
+        <section className="home">
             <h2>Welcome to my books app</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non, veritatis?</p>
+            <p>This is my first React application and i hope you enjoy and buy yourself some books. </p>
+            <img className="icon" src="assets/style/img/bookshelf.png" alt="" />
         </section>
     )
 }
